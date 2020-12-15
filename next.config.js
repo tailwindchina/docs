2aced20a6b9449a9ff106d1c1000055ebc7a944d
codesinging <codesinging@gmail.com>
2020-12-16 00:07:47 +0800

    export docs

--- next.config.js
+++ next.config.js
@@ -26,7 +26,6 @@ const fallbackDefaultExports = {
 
 module.exports = withBundleAnalyzer({
   pageExtensions: ['js', 'jsx', 'mdx'],
-  basePath: '/tailwindcss-cn',
   experimental: {
     modern: true,
   },
