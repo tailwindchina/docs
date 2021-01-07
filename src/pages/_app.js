33889defe246a102496d42ad200b771f63946e5b
neoney <zairl23@126.com>
2021-01-07 19:18:48 +0800

    Add head meta info: description, keywords, author, update title suffix.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -63,7 +63,7 @@ export default function App({ Component, pageProps, router }) {
 
   return (
     <>
-      <Title suffix="Tailwind CSS">{meta.metaTitle || meta.title}</Title>
+      <Title suffix="中文文档 For TailwindCSS">{meta.metaTitle || meta.title}</Title>
       <Head>
         <meta key="twitter:card" name="twitter:card" content="summary_large_image" />
         <meta key="twitter:site" name="twitter:site" content="@tailwindcss" />
