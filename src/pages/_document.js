6a09fc3c187bd086118dd8d335e8683efc5b4c27
neoney <zairl23@126.com>
2021-01-07 12:15:47 +0800

    Add google analytics support

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,5 @@
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
+import { GA_TRACKING_ID } from '../utils/gtag'
 
 export default class Document extends NextDocument {
   static async getInitialProps(ctx) {
@@ -15,6 +16,23 @@ export default class Document extends NextDocument {
         }`}
       >
         <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
           <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
